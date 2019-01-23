6aeecb10dc040690b78570e1ea930f5bf20e17af
Rob <rob.nichols@verizon.net>
2019-01-23 16:25:56 -0500

    Updated admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Author
+from .models import Book
+from .models import Sale
+from .models import Publisher
+from .models import Customer
+from .models import Price
 
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Sale)
+admin.site.register(Publisher)
+admin.site.register(Customer)
+admin.site.register(Price)
 # Register your models here.
