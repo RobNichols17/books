4bf1580a3d2ee91e4953cf185ca31ad2b974c87a
Rob Nichols <rob.nichols@verizon.net>
2019-01-25 16:19:01 -0500

    Added author list

--- myapp/views.py
+++ myapp/views.py
@@ -4,4 +4,4 @@ from .models import Author
 # Create your views here.
 def author_list(request):
     authors = Author.objects.order_by('lastnane')
-    return render(request, 'myapp/author_list.html',{'authors' : firstname + ' ' + lastname})
+    return render(request, 'myapp/author_list.html',{'authors' : author})
