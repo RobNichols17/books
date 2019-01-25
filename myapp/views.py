026962cbf630b9be1891d3e3d13395683c11f2e5
Rob Nichols <rob.nichols@verizon.net>
2019-01-25 16:09:18 -0500

    Added author list

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Author
 
 # Create your views here.
 def author_list(request):
-    return render(request, 'myapp/author_list.html',{})
+    authors = Author.objects.order_by('lastnane')
+    return render(request, 'myapp/author_list.html',{'authors' : firstname + ' ' + lastname})
