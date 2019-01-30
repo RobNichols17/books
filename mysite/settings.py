c04abd15cbef71286f6d62c8b94a1ad1ea312943
Rob <Rob.nichols@verizon.net>
2019-01-30 15:01:50 +0000

    Next

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,4 +127,5 @@ STATIC_URL = os.path.join(BASE_DIR,'myapp/static/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'myapp/static/'),
+        '/var/www/',
 )
